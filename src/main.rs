f834e24d52055a92d2f6a17dde830a4749eb41e1
Maneac <benatkinson027@gmail.com>
2020-12-03 09:37:13 +0000

    Day 3

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 mod day_1;
 mod day_2;
+mod day_3;
 
 fn main() {
     println!("Day 1");
     day_1::run();
     println!("Day 2");
     day_2::run();
+    println!("Day 3");
+    day_3::run();
 }
